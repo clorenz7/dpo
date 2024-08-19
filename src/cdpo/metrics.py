ffe9ea8a22df1ea6e631a7eb6d50d824add63d8c
Cory Lorenz <clorenz@gmail.com>
2024-08-19 10:26:31 -0700

    Add ticks and grid lines to plots

--- src/cdpo/metrics.py
+++ src/cdpo/metrics.py
@@ -3,12 +3,16 @@ import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 
 
-def plot_validation_curves(metrics, step_size=100, show_plot=False, save_plot=''):
+def plot_validation_curves(metrics, step_size=100, show_plot=False, save_plot='',
+                           log_loss=False):
 
     losses, win_rates, prob_deltas = [], [], []
+    tr_losses = []
 
     for data in metrics:
         if 'eval_loss' not in data:
+            if 'loss' in data:
+                tr_losses.append(data['loss'])
             continue
 
         losses.append(data['eval_loss'])
@@ -30,20 +34,34 @@ def plot_validation_curves(metrics, step_size=100, show_plot=False, save_plot=''
     if n_plots >= 1:
         plt.subplot(n_plots, 1, 1)
         p_idx = 1
-    plt.plot(steps, losses)
+    if log_loss:
+        plt.semilogy(steps, losses, '.b-', steps[1:], tr_losses, '.g-')
+    else:
+        plt.plot(steps, losses, '.b-', steps[1:], tr_losses, '.g-')
+
     plt.ylabel('Loss')
+    plt.grid(True, axis='y')
+    plt.minorticks_on()
+    plt.grid(True, which='minor', axis='y', linestyle=':', linewidth=0.5)
+    plt.legend(['Val Set', 'Train'])
 
     if len(win_rates) > 0:
         p_idx += 1
         plt.subplot(n_plots, 1, p_idx)
-        plt.plot(steps, win_rates)
+        plt.plot(steps, win_rates, '.b-')
         plt.ylabel('Win Rate vs. Reject')
+        plt.grid(True, axis='y')
+        plt.minorticks_on()
+        plt.grid(True, which='minor', axis='y', linestyle=':', linewidth=0.5)
 
     if len(prob_deltas) > 0:
         p_idx += 1
         plt.subplot(n_plots, 1, p_idx)
-        plt.plot(steps, prob_deltas)
+        plt.plot(steps, prob_deltas, '.b-')
         plt.ylabel(r'$\log \pi_W / \pi_L$')
+        plt.grid(True, axis='y')
+        plt.minorticks_on()
+        plt.grid(True, which='minor', axis='y', linestyle=':', linewidth=0.5)
     plt.xlabel('Step')
 
     if save_plot:
